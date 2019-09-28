cf21e50cf4cf02b40923c9ae343192b6c529979c
abhay5075 <42096870+abhay5075@users.noreply.github.com>
2019-09-28 13:12:16 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+month_conversions={"Jan":"January",
+                   "Feb":"Febuary",
+                   "Mar":"March",
+                   "Apr":"April",
+                   "May":"May",
+                   "Jun":"June",
+                   "July":"July",
+                   "Aug":"August",
+                   "Sep":"September",
+                   "Oct":"October",
+                   "Nov":"November",
+                   "Dec":"December"
+                      }
+print(month_conversions.get("Mare","not a valid entry"))
